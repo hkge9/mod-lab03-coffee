870befe8b5089b44ff646f39f921637e00d2cd96
Vladislav Melnikov <ninthofhokage@gmail.com>
2025-04-06 18:39:09 +0300

    Change Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,79 +1,83 @@
+// Copyright 2025 9hkge
+
 #include "Automata.h"
 
-Automata::Automata(){
+#include <vector>   
+#include <string>   
+
+Automata::Automata() {
   cash = 0;
   menu = {"Espresso", "Cappuccino", "Latte", "Americano", "Raf"};
   prices = {130, 150, 170, 120, 200};
   state = OFF;
 }
 
-void Automata::on(){
-  if (state == OFF){
+void Automata::on() {
+  if (state == OFF) {
     state = WAIT;
   }
 }
 
-void Automata::off(){
-  if (state == WAIT){
+void Automata::off() {
+  if (state == WAIT) {
     state = OFF;
   }
 }
 
 void Automata::coin(int csh) {
-    if (state == WAIT || state == ACCEPT) {
-        if (csh > 0) {
-            cash += csh;
-            state = ACCEPT;
-        }
-        else {
-            cancel();
-        }
+  if (state == WAIT || state == ACCEPT) {
+    if (csh > 0) {
+      cash += csh;
+      state = ACCEPT;
+    } else {
+      cancel();
     }
-  
+  }
 }
 
-vector <string> Automata::getMenu(){
-  vector <string> menuAndPrices;
-  for (int i = 0; i < menu.size(); i++){
-    menuAndPrices.push_back(menu[i] + " " + to_string(prices[i]));
+std::vector<std::string> Automata::getMenu() {
+  std::vector<std::string> menuAndPrices;
+  for (int i = 0; i < menu.size(); i++) {
+    menuAndPrices.push_back(menu[i] + " " + std::to_string(prices[i]));
   }
- return menuAndPrices;
+  return menuAndPrices;
 }
 
 void Automata::cancel() {
-    state = WAIT;
+  state = WAIT;
 }
 
 STATES Automata::getState() {
-    return state;
+  return state;
 }
 
-void Automata::choice(string drink) {
-    bool status = false;
-    if (state == ACCEPT || state == CHECK) {
-        state = CHECK;
-    }
-    for (int i = 0; i < menu.size(); i++) {
-        if (menu[i] == drink && cash >= prices[i]) {
-            cash = cash - prices[i];
-            status = true;
-        }
-    }
-    if (status == false) {
-        cancel();
-    }
+void Automata::choice(std::string drink) {
+  bool status = false;
+  if (state == ACCEPT || state == CHECK) {
+    state = CHECK;
+  }
 
+  for (int i = 0; i < menu.size(); i++) {
+    if (menu[i] == drink && cash >= prices[i]) {
+      cash -= prices[i];
+      status = true;
+    }
+  }
+
+  if (!status) {
+    cancel();
+  }
 }
 
 void Automata::cook() {
-    if (state == CHECK) {
-        state = COOK;
-    }
-    finish();
+  if (state == CHECK) {
+    state = COOK;
+  }
+  finish();
 }
 
 void Automata::finish() {
-    if (state == COOK) {
-        state = WAIT;
-    }
+  if (state == COOK) {
+    state = WAIT;
+  }
 }
