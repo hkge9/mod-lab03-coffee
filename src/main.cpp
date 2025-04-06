d100f78095629caf27c271edad65bd87ec1e6857
Vladislav Melnikov <ninthofhokage@gmail.com>
2025-04-06 18:06:15 +0300

    Added main to demonstrate the operation of the machine

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,23 @@
 
 int main(){
 
+  vector <string> st {"OFF", "WAIT", "ACCEPT", "CHECK", "COOK"};
   Automata coffee;
-
+  coffee.on();
+  cout << st[coffee.getState()] << endl;
+  vector <string> testMenu = coffee.getMenu();
+  for (string val : testMenu) 
+  {
+	  cout << val << " ";
+  }
+  coffee.coin(200);
+  cout << st[coffee.getState()] << endl;
+  coffee.choice("Espresso");
+  cout << st[coffee.getState()] << endl;
+  coffee.cook();
+  cout << st[coffee.getState()] << endl;
+  coffee.off();
+  cout << st[coffee.getState()] << endl;
   return 0;
+
 }
